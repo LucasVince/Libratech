700e68d95fcc246d1f448139680d7f859ff74169
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-08-23 11:39:54 -0300

    Add files via upload

---
+++ iframe/script js/livros.js
@@ -0,0 +1,31 @@
+for (let i = 1; i <= 5000; i++) {
+    const main = document.querySelector('main');
+    const div = document.createElement('div');
+    const div_id = document.createElement('div');
+    const icon = document.createElement('ion-icon');
+    const p_id = document.createElement('p');
+    const p_div = document.createElement('p');
+    const link = document.createElement('a');
+    const btn = document.createElement('button');
+    
+    let id = document.createTextNode(i);
+    let divText = document.createTextNode('Nome Livro');
+    let btnText = document.createTextNode('consultar');
+
+    div.setAttribute('class', 'livro');
+    div_id.setAttribute('class', 'livro_id');
+    icon.setAttribute('name', 'book');
+
+    main.appendChild(div);
+    div.appendChild(div_id);
+    div_id.appendChild(icon);
+    div_id.appendChild(p_id);
+    p_id.appendChild(id);
+    div.appendChild(p_div);
+    p_div.appendChild(divText);
+    div.appendChild(link);
+    link.appendChild(btn);
+    btn.appendChild(btnText);
+}
+
+//sera implementadoo posteriormente com nodejs
