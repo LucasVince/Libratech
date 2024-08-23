d4d4b98250f637584a288eb41033062e97eb1d0c
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-08-23 11:53:27 -0300

    Update database.sql

--- database/database.sql
+++ database/database.sql
@@ -8,7 +8,7 @@ CREATE TABLE livros (
     id int NOT NULL AUTO_INCREMENT,
     titulo varchar(50) NOT NULL,
     subtitulo varchar(50),
-    clasificação_indicativa int NOT NULL,
+    clasificação_indicativa enum('infanto juvenil','encino medio') NOT NULL,
     autor varchar(50) NOT NULL,
     genero enum('romance', 'ficção'),
     PRIMARY KEY(id)
@@ -17,7 +17,7 @@ CREATE TABLE livros (
 CREATE TABLE alunos (
     id int NOT NULL AUTO_INCREMENT,
     nome varchar(100) NOT NULL,
-    iade int NOT NULL,
+    claificação enum('infanto juvenil','encino medio') NOT NULL,
     email varchar(75) NOT NULL,
     endereco varchar(100) NOT NULL,
     tel char(11),
