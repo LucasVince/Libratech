e017d1285db1e345aa8826c258a8a08f9484646e
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-09-13 12:19:46 -0300

    Add files via upload

--- iframe/js/modo_escuro.js
+++ iframe/js/modo_escuro.js
@@ -1,6 +1,66 @@
-const switchInput = document.querySelector('#checkboxInput');
-let modoEscuro = false;
+window.addEventListener('DOMContentLoaded', () => {
+    const switchInput = document.querySelector('#checkboxInput');
+    const tagMain = document.querySelector('main');
+    const tagH1 = document.querySelector('h1');
+    const divsConfig = document.querySelector('#configs div');
+    const tagBody = document.querySelector('body');
 
-if (modoEscuro) {
+    const mudarParaModoEscuro = () => {
+        if (tagMain) tagMain.style.backgroundColor = '#2e2e29';
+        if (tagH1) {
+            tagH1.style.backgroundColor = '#5a5a51';
+            tagH1.style.color = 'white';
+        }
+        if (divsConfig) {
+            divsConfig.style.backgroundColor = '#5a5a51';
+            divsConfig.style.color = 'white';
+        }
+        if (tagBody) tagBody.style.backgroundColor = '#2e2e29';
+    }
     
-}
+    const mudarParaModoClaro = () => {
+        if (tagMain) tagMain.style.backgroundColor = '#a9a9a4';
+        if (tagH1) {
+            tagH1.style.backgroundColor = '#e6e6e1';
+            tagH1.style.color = 'black';
+        }
+        if (divsConfig) {
+            divsConfig.style.backgroundColor = '#e6e6e1';
+            divsConfig.style.color = 'black';
+        }
+
+        if (tagBody) tagBody.style.backgroundColor = '#a9a9a4';
+    }
+
+    const switchDelay = () => {
+        switchInput.disabled = true;
+        setTimeout(() => {
+            switchInput.disabled = false;
+        }, 500);
+    }
+
+    const modoEscuro = () => {
+        if (switchInput.checked) {
+            switchDelay();
+            mudarParaModoEscuro();
+            localStorage.setItem('estadoModoEscuro', 'true');
+        } else {
+            switchDelay();
+            mudarParaModoClaro();
+            localStorage.setItem('estadoModoEscuro', 'false');
+        }
+    }
+
+    if (switchInput) {
+        const modoEscuroSalvo = localStorage.getItem('estadoModoEscuro');
+
+        if (modoEscuroSalvo === 'true') {
+            switchInput.checked = true;
+            mudarParaModoEscuro();
+        } else {
+            switchInput.checked = false;
+            mudarParaModoClaro();
+        }
+        switchInput.addEventListener('change', modoEscuro);
+    }
+});
