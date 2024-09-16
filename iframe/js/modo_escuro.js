6b1199d86de9d2d1ca052779bdb1a429f99980e4
Lucas <154390549+LucasVince@users.noreply.github.com>
2024-09-16 13:29:52 -0300

    Add files via upload

--- iframe/js/modo_escuro.js
+++ iframe/js/modo_escuro.js
@@ -1,14 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
     const switchInput = document.querySelector('#checkboxInput');
 
-    const tagMain = document.querySelector('main');
+    const tagMainConfig = document.querySelector('#mainConfig');
     const tagH1 = document.querySelector('h1');
     const configsDiv = document.querySelector('#configs div');
     
+    const TagBodyLivro = document.querySelector('#bodyLivro');
     const divLivros = document.querySelectorAll('.livro');
+    const divSearch = document.querySelector('#search-div');
 
     const mudarParaModoEscuro = () => {
-        if (tagMain) tagMain.style.backgroundColor = '#161718';
+        if (tagMainConfig) tagMainConfig.style.backgroundColor = '#161718';
         if (tagH1) {
             tagH1.style.backgroundColor = '#474a4d';
             tagH1.style.color = 'white';
@@ -17,24 +19,40 @@ window.addEventListener('DOMContentLoaded', () => {
             configsDiv.style.backgroundColor = '#474a4d';
             configsDiv.style.color = 'white';
         }
+
+        if (TagBodyLivro) {
+            TagBodyLivro.style.backgroundColor = '#161718';
+        }
         if (divLivros) {
             console.log(divLivros)
-            divLivros.forEach(el => divLivros[el].style.backgroundColor = '#474a4d');
+            divLivros.forEach(el => el.style.boxShadow = '0 2px 0 #474a4d');
+            divLivros.forEach(el => el.style.color = 'white');
+        }
+        if (divSearch) {
+            divSearch.style.boxShadow = '0 2px 0 #474a4d';
         }
     }
 
     const mudarParaModoClaro = () => {
-        if (tagMain) tagMain.style.backgroundColor = '#a7a7a1';
+        if (tagMainConfig) tagMainConfig.style.backgroundColor = '#a7a7a1';
         if (tagH1) {
-            tagH1.style.backgroundColor = '#e6e6e1';
+            tagH1.style.backgroundColor = '#dadad5';
             tagH1.style.color = 'black';
         }
         if (configsDiv) {
-            configsDiv.style.backgroundColor = '#e6e6e1';
+            configsDiv.style.backgroundColor = '#dadad5';
             configsDiv.style.color = 'black';
         }
+
+        if (TagBodyLivro) {
+            TagBodyLivro.style.backgroundColor = '#dadad5';
+        }
         if (divLivros) {
-            divLivros.forEach(el => divLivros[el].style.backgroundColor = '#dadad5');
+            divLivros.forEach(el => el.style.backgroundColor = '0 2px 0 #acacac');
+            divLivros.forEach(el => el.style.color = 'black');
+        }
+        if (divSearch) {
+            divSearch.style.boxShadow = '0 2px 0 #acacac';
         }
     }
 
